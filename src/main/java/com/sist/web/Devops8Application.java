f9c24211058ed0dce2d534a5b9736af56eb4735c
chaijewon <vcandjava@nate.com>
2025-06-25 09:02:20 +0900

    first

---
+++ src/main/java/com/sist/web/Devops8Application.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops8Application.class, args);
+	}
+
+}
